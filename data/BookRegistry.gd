85bcbeeb3683a61f82e6f1ef7370d8635e399cba
Henry Conklin <henrywconklin@gmail.com>
2021-11-28 11:05:22 -0500

    Move BookRegistryto singleton, move Shelf to instanced scene

--- data/BookRegistry.gd
+++ data/BookRegistry.gd
@@ -1,17 +1,6 @@
-tool
-extends Resource
-class_name BookRegistry
+extends Node
 
-# Map from cell position to MultiMesh for that cell
-export(Dictionary) var bookCells
-# BigInt object, position of cell (0,0,0)
-export(Resource) var centerCell
-
-export(int, 0, 10) var numShelves = 5
-export(int, 0, 100) var booksPerShelf = 50
-export(float, 0, 10) var shelfSpacing = .4
-export(float, 0, 1) var bookSpacing = .005
-export(Mesh) var bookMesh: Mesh
+const params = preload("res://data/book_registry_params.tres")
 
 func makeDefaultShelf() -> MultiMesh:
 	var shelfMesh: MultiMesh = MultiMesh.new()
@@ -19,18 +8,18 @@ func makeDefaultShelf() -> MultiMesh:
 	shelfMesh.custom_data_format = MultiMesh.CUSTOM_DATA_FLOAT
 	shelfMesh.transform_format = MultiMesh.TRANSFORM_3D
 	
-	shelfMesh.mesh = bookMesh
-	shelfMesh.instance_count = numShelves * booksPerShelf
+	shelfMesh.mesh = params.bookMesh
+	shelfMesh.instance_count = params.numShelves * params.booksPerShelf
 	
-	var bookSize = bookMesh.get_aabb().size
-	for i in range(numShelves):
-		for j in range(booksPerShelf):
-			var ind = i * booksPerShelf + j
-			var zoff = ((j - booksPerShelf / 2) * (bookSize.z + bookSpacing) 
-						+ (bookSize.z + bookSpacing) / 2)
+	var bookSize = params.bookMesh.get_aabb().size
+	for i in range(params.numShelves):
+		for j in range(params.booksPerShelf):
+			var ind = i * params.booksPerShelf + j
+			var zoff = ((j - params.booksPerShelf / 2) * (bookSize.z + params.bookSpacing) 
+						+ (bookSize.z + params.bookSpacing) / 2)
 			var offset = Vector3(
 				0.0,
-				shelfSpacing * i + bookSize.y / 2,
+				params.shelfSpacing * i + bookSize.y / 2,
 				zoff)
 			var transform = Transform.IDENTITY.translated(offset)
 			shelfMesh.set_instance_transform(ind, transform)
@@ -38,15 +27,15 @@ func makeDefaultShelf() -> MultiMesh:
 	return shelfMesh
 	
 func defaultShelfAABB() -> AABB:
-	var bookSize = bookMesh.get_aabb().size
+	var bookSize = params.bookMesh.get_aabb().size
 	var position = Vector3(
 			-bookSize.x/2,
 			0,
-			-(float(booksPerShelf) / 2.0) * (bookSize.z + bookSpacing)
+			-(float(params.booksPerShelf) / 2.0) * (bookSize.z + params.bookSpacing)
 			)
 	var size = Vector3(
 		bookSize.x,
-		 numShelves * shelfSpacing,
-		booksPerShelf * (bookSize.z + bookSpacing) 
+		(params.numShelves) * params.shelfSpacing - (params.shelfSpacing - bookSize.y),
+		params.booksPerShelf * (bookSize.z + params.bookSpacing) 
 	)
 	return AABB(position, size)
--- props/Shelf.gd
+++ props/Shelf.gd
@@ -1,12 +1,21 @@
-tool
 extends MultiMeshInstance
 
-export(Resource) var bookRegistry
 export(bool) var regenerate = false setget set_regenerate
+export(NodePath) var collisionShape : NodePath
+
+func _ready():
+	regenerate_multimesh()
 
 func set_regenerate(val):
 	if val:
-		multimesh = bookRegistry.makeDefaultShelf()
-		set_custom_aabb(bookRegistry.defaultShelfAABB())
-		
+		regenerate_multimesh()
 	regenerate = val
+
+func regenerate_multimesh():
+	multimesh = BookRegistry.makeDefaultShelf()
+	var aabb : AABB = BookRegistry.defaultShelfAABB()
+	set_custom_aabb(aabb)
+	var shape : CollisionShape = get_node(collisionShape)
+	shape.shape = BoxShape.new()
+	shape.shape.extents = aabb.size / 2
+	shape.translation.y = aabb.size.y / 2
