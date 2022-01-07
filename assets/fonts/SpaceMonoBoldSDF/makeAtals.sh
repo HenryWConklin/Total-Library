d607b1e3dee705e5ed8e102f5bbffcbdd2f4f83e
Henry Conklin <henrywconklin@gmail.com>
2022-01-07 09:29:21 -0800

    Space Mono Bold SDF font

---
+++ assets/fonts/SpaceMonoBoldSDF/makeAtals.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Montage command from imagemagick
+montage {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}.png dot.png ",.png" "?.png" "!.png" -geometry +2+2 -background none SpaceMonoBoldSDF.png
+
