09bdf96478ebe69a85759cb57815267ed7fa3bc9
Henry Conklin <henrywconklin@gmail.com>
2022-02-13 11:47:43 -0800

    Add precommit script for formatting/linting gdscript

---
+++ util/gdlint-precommit.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "Checking gdscript formatting"
+if ! git diff --cached --name-only --diff-filter=AM $against | grep ".gd$" | xargs --no-run-if-empty gdformat
+then
+  echo "Formatting issues detected, update with formatting before commiting"
+  exit 1
+fi
+
+echo "Checking gdscript lints"
+if ! git diff --cached --name-only --diff-filter=AM $against | grep ".gd$" | xargs --no-run-if-empty gdlint
+then
+  echo "Lint issues detected, fix before commiting"
+  exit 1
+fi
