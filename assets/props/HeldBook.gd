ec695a14b56120c7ee0f6b751317c8fffbd81d9e
Henry Conklin <henrywconklin@gmail.com>
2022-02-27 18:52:16 -0800

    Pick books from shelves

---
+++ assets/props/HeldBook.gd
@@ -0,0 +1,114 @@
+extends Spatial
+
+enum State { NONE, ANIMATING_PICK, HELD, ANIMATING_DROP }
+
+export(NodePath) var animation_player_path: NodePath
+export(NodePath) var tween_path: NodePath
+export(NodePath) var display_path: NodePath
+export(NodePath) var mesh_path: NodePath
+export(float) var pull_animation_time: float = 1
+export(float) var pull_animation_shelf_clearence: float = 0.3
+export(PackedScene) var floor_book_scene
+
+var state: int = State.NONE setget set_state
+
+var _book_text = null
+var _page: int = 0
+var _start_transform: Transform = Transform.IDENTITY
+var _mid_transform: Transform = Transform.IDENTITY
+
+onready var animation_player: AnimationPlayer = get_node(animation_player_path)
+onready var tween: Tween = get_node(tween_path)
+onready var display_node: Spatial = get_node(display_path)
+onready var book_material: Material = get_node(mesh_path).get_active_material(0)
+
+
+func _ready():
+	self.state = State.NONE
+
+
+func set_state(new_state: int):
+	match new_state:
+		State.NONE:
+			display_node.visible = false
+		State.ANIMATING_PICK:
+			assert(state == State.NONE)
+			display_node.visible = true
+		State.HELD:
+			assert(state == State.ANIMATING_PICK)
+		State.ANIMATING_DROP:
+			assert(state == State.HELD)
+	state = new_state
+
+
+func can_pick_up_book() -> bool:
+	return state == State.NONE
+
+
+func _set_animation_progress(p: float):
+	if p < 0.5:
+		display_node.global_transform = _start_transform.interpolate_with(_mid_transform, p * 2)
+	else:
+		display_node.global_transform = _mid_transform.interpolate_with(
+			display_node.get_parent().global_transform, (p - 0.5) * 2
+		)
+
+
+func pull_from_shelf(book_text, book_transform_local: Transform, shelf_transform_global: Transform):
+	assert(can_pick_up_book())
+	yield(_animate_pick(book_text, book_transform_local, shelf_transform_global), "completed")
+
+
+func can_drop_book() -> bool:
+	return state == State.HELD
+
+
+func drop_book() -> BookText:
+	assert(can_drop_book())
+	yield(_animate_drop(), "completed")
+	return _book_text
+
+
+func _animate_pick(book_text, book_transform_local: Transform, shelf_transform_global: Transform):
+	set_state(State.ANIMATING_PICK)
+	_book_text = book_text
+	var packed_title = BookRegistry.get_title(book_text)
+	book_material.set_shader_param("title1", int(packed_title.r))
+	book_material.set_shader_param("title2", int(packed_title.g))
+	book_material.set_shader_param("title3", int(packed_title.b))
+	book_material.set_shader_param("title4", int(packed_title.a))
+
+	var shelf_to_local_transform = shelf_transform_global
+	_start_transform = shelf_to_local_transform * book_transform_local
+	_mid_transform = (
+		shelf_to_local_transform
+		* book_transform_local.translated(Vector3.RIGHT * pull_animation_shelf_clearence)
+	)
+
+	# Reset animation
+	animation_player.stop()
+	_set_animation_progress(0)
+
+	assert(
+		tween.interpolate_method(
+			self,
+			"_set_animation_progress",
+			0,
+			1,
+			pull_animation_time,
+			Tween.TRANS_QUAD,
+			Tween.EASE_IN_OUT
+		)
+	)
+	assert(tween.start())
+	yield(tween, "tween_completed")
+	set_state(State.HELD)
+	animation_player.play("BookOpen")
+
+
+func _animate_drop():
+	set_state(State.ANIMATING_DROP)
+	animation_player.play_backwards("BookOpen")
+	yield(animation_player, "animation_finished")
+	# TODO Call BookRegistry to make rigid body
+	set_state(State.NONE)
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+const HeldBook = preload("res://assets/props/HeldBook.gd")
+
 const SNAP_VECTOR: Vector3 = Vector3(0, -1, 0)
 const UP: Vector3 = Vector3(0, 1, 0)
 
@@ -8,18 +10,17 @@ export(Vector2) var move_speed = Vector2(5, 5)
 export(float) var gravity = 10
 export(NodePath) var camera_path: NodePath
 export(NodePath) var raycast_path: NodePath
+export(NodePath) var held_book_path: NodePath
 
 var _mouse_move = Vector2(0, 0)
+
 onready var camera: Camera = get_node(camera_path)
 onready var raycast: RayCast = get_node(raycast_path)
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+onready var held_book: HeldBook = get_node(held_book_path)
 
 
 func _unhandled_input(event: InputEvent):
+	# Mouse capture/uncapture
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		var mouse_motion = event.get_relative()
 		_mouse_move += mouse_motion
@@ -34,6 +35,33 @@ func _unhandled_input(event: InputEvent):
 	):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		get_tree().set_input_as_handled()
+
+	# Book pickup
+	if (
+		event.is_action_pressed("pick_up")
+		and raycast.is_colliding()
+		and raycast.get_collider() is Area
+		and held_book.can_pick_up_book()
+	):
+		var picked_shelf_collision = raycast.get_collider()
+		var picked_shelf = picked_shelf_collision.get_node(picked_shelf_collision.shelf_path)
+		var picked_point_global = raycast.get_collision_point()
+		var picked_point_local = picked_shelf_collision.to_local(picked_point_global)
+		var shelf_ind = picked_shelf.shelf_index
+		var book_text = BookRegistry.get_book_text_at_point(shelf_ind, picked_point_local)
+		if book_text == null:
+			return
+
+		# Should probably move this into BookRegistry
+		var book_transform_local = picked_shelf.multimesh.get_instance_transform(book_text.book)
+		book_transform_local.basis = Basis.IDENTITY
+
+		held_book.pull_from_shelf(
+			book_text, book_transform_local, picked_shelf_collision.global_transform
+		)
+		get_tree().set_input_as_handled()
+	elif event.is_action_pressed("pick_up") and held_book.can_drop_book():
+		held_book.drop_book()
 
 
 func _handle_movement():
@@ -59,9 +87,11 @@ func _handle_movement():
 	move_vec = move_vec.rotated(-self.rotation.y)
 
 	if self.is_on_floor():
-		self.move_and_slide_with_snap(Vector3(move_vec.x, 0, move_vec.y), SNAP_VECTOR, UP, true)
+		var _move_result = self.move_and_slide_with_snap(
+			Vector3(move_vec.x, 0, move_vec.y), SNAP_VECTOR, UP, true
+		)
 	else:
-		self.move_and_slide(Vector3(move_vec.x, -gravity, move_vec.y), UP, true)
+		var _move_result = self.move_and_slide(Vector3(move_vec.x, -gravity, move_vec.y), UP, true)
 
 
 func _physics_process(_delta):
