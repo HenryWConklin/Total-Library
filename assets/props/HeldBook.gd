b6e668c91d96c72735d12b9569a5d243d39b1697
Henry Conklin <henrywconklin@gmail.com>
2022-03-05 17:21:37 -0800

    Add animation for page turns

--- assets/props/HeldBook.gd
+++ assets/props/HeldBook.gd
@@ -1,11 +1,13 @@
 extends Spatial
 
-enum State { NONE, ANIMATING_PICK, HELD, ANIMATING_DROP }
+enum State { NONE, ANIMATING_PICK, HELD, ANIMATING_DROP, ANIMATING_TURN }
 
-export(NodePath) var animation_player_path: NodePath
+export(NodePath) var book_open_animation_player_path: NodePath
+export(NodePath) var page_turn_animation_player_path: NodePath
+export(NodePath) var page_mesh_path: NodePath
+export(NodePath) var book_mesh_path: NodePath
 export(NodePath) var tween_path: NodePath
 export(NodePath) var display_path: NodePath
-export(NodePath) var mesh_path: NodePath
 export(float) var pull_animation_time: float = 1
 export(float) var pull_animation_shelf_clearence: float = 0.3
 export(PackedScene) var floor_book_scene
@@ -18,29 +20,39 @@ var _page: int = 0
 var _start_transform: Transform = Transform.IDENTITY
 var _mid_transform: Transform = Transform.IDENTITY
 
-onready var animation_player: AnimationPlayer = get_node(animation_player_path)
+onready var book_open_animation_player: AnimationPlayer = get_node(book_open_animation_player_path)
+onready var page_turn_animation_player: AnimationPlayer = get_node(page_turn_animation_player_path)
 onready var tween: Tween = get_node(tween_path)
 onready var display_node: Spatial = get_node(display_path)
-onready var book_material: Material = get_node(mesh_path).get_active_material(0)
+onready var book_material: Material = get_node(book_mesh_path).get_active_material(0)
+onready var page_mesh: Spatial = get_node(page_mesh_path)
 
 
 func _ready():
 	self.state = State.NONE
 	book_material.set_shader_param("page1_text_texture", get_node(page_renderers[0]).get_texture())
 	book_material.set_shader_param("page2_text_texture", get_node(page_renderers[1]).get_texture())
+	var page_material = page_mesh.get_active_material(0)
+	page_material.set_shader_param("back_page", get_node(page_renderers[2]).get_texture())
+	page_material.set_shader_param("front_page", get_node(page_renderers[3]).get_texture())
 
 
 func set_state(new_state: int):
 	match new_state:
 		State.NONE:
 			display_node.visible = false
+			page_mesh.visible = false
 		State.ANIMATING_PICK:
 			assert(state == State.NONE)
 			display_node.visible = true
 		State.HELD:
-			assert(state == State.ANIMATING_PICK)
+			page_mesh.visible = false
+			assert(state == State.ANIMATING_PICK or state == State.ANIMATING_TURN)
 		State.ANIMATING_DROP:
 			assert(state == State.HELD)
+		State.ANIMATING_TURN:
+			assert(state == State.HELD)
+			page_mesh.visible = true
 	state = new_state
 
 
@@ -81,7 +93,7 @@ func _animate_pick(book_text, book_transform_local: Transform, shelf_transform_g
 	book_material.set_shader_param("title3", int(packed_title.b))
 	book_material.set_shader_param("title4", int(packed_title.a))
 	_page = 0
-	_update_page_renderers()
+	_update_page_renderers_held()
 
 	var shelf_to_local_transform = shelf_transform_global
 	_start_transform = shelf_to_local_transform * book_transform_local
@@ -91,7 +103,7 @@ func _animate_pick(book_text, book_transform_local: Transform, shelf_transform_g
 	)
 
 	# Reset animation
-	animation_player.stop()
+	book_open_animation_player.stop()
 	_set_animation_progress(0)
 
 	assert(
@@ -108,42 +120,67 @@ func _animate_pick(book_text, book_transform_local: Transform, shelf_transform_g
 	assert(tween.start())
 	yield(tween, "tween_completed")
 	set_state(State.HELD)
-	animation_player.play("BookOpen")
+	book_open_animation_player.play("BookOpen")
 
 
 func _animate_drop():
 	set_state(State.ANIMATING_DROP)
-	animation_player.play_backwards("BookOpen")
-	yield(animation_player, "animation_finished")
+	book_open_animation_player.play_backwards("BookOpen")
+	yield(book_open_animation_player, "animation_finished")
 	# TODO Call BookRegistry to make rigid body
 	set_state(State.NONE)
 
 
-func _update_page_renderers():
-	var page1 = BookRegistry.get_page(_book_text, _page)
-	get_node(page_renderers[0]).set_text(page1)
-	if _page + 1 >= BookRegistry.PARAMS.pages_per_book:
-		get_node(page_renderers[1]).set_text("")
+func _set_page_renderer_text(renderer: int, page: int):
+	if page < 0 or page >= BookRegistry.PARAMS.pages_per_book:
+		get_node(page_renderers[renderer]).set_text("")
 	else:
-		var page2 = BookRegistry.get_page(_book_text, _page + 1)
-		get_node(page_renderers[1]).set_text(page2)
+		var text = BookRegistry.get_page(_book_text, page)
+		get_node(page_renderers[renderer]).set_text(text)
+
+
+func _update_page_renderers_held():
+	_set_page_renderer_text(0, _page)
+	_set_page_renderer_text(1, _page + 1)
+
+
+func _update_page_renderers_turn():
+	_set_page_renderer_text(0, _page)
+	_set_page_renderer_text(1, _page + 3)
+	_set_page_renderer_text(2, _page + 1)
+	_set_page_renderer_text(3, _page + 2)
 
 
 func can_turn_page() -> bool:
 	return state == State.HELD
 
 
-func page_forward():
+func _page_turn(diff: int):
 	assert(can_turn_page())
-	if _page >= BookRegistry.PARAMS.pages_per_book - 2:
+	if _page + diff >= BookRegistry.PARAMS.pages_per_book:
+		# yield then return so this is always a coroutine
+		yield()
 		return
-	_page += 2
-	_update_page_renderers()
+	elif _page + diff < 0:
+		yield()
+		return
+	set_state(State.ANIMATING_TURN)
+	if diff > 0:
+		_update_page_renderers_turn()
+		_page += diff
+		page_turn_animation_player.play("PageTurn")
+	else:
+		_page += diff
+		_update_page_renderers_turn()
+		page_turn_animation_player.play_backwards("PageTurn")
+	yield(page_turn_animation_player, "animation_finished")
+	set_state(State.HELD)
+	_update_page_renderers_held()
+
+
+func page_forward():
+	yield(_page_turn(2), "completed")
 
 
 func page_back():
-	assert(can_turn_page())
-	if _page == 0:
-		return
-	_page -= 2
-	_update_page_renderers()
+	yield(_page_turn(-2), "completed")
--- scripts/PageRenderer.gd
+++ scripts/PageRenderer.gd
@@ -10,7 +10,7 @@ onready var label: Label = get_node(label_path)
 func _ready():
 	# Assumes monospaced font, i.e. all chars have same width
 	var char_size = label.get_font("font").get_char_size(ord("A"))
-	print(char_size)
+
 	chars_per_line = int(label.rect_size.x / char_size.x)
 
 
