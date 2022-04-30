65731149fd9efee84af01098a0cf86712d4d9fb1
Henry Conklin <henrywconklin@gmail.com>
2022-04-30 07:14:45 -0700

    Fix shadow settings signal in floor book

--- assets/props/FloorBook.gd
+++ assets/props/FloorBook.gd
@@ -12,6 +12,8 @@ func _ready():
 	material.set_shader_param("packed_title4", int(title.a))
 	material.set_shader_param("use_packed_title", true)
 	$MeshInstance.set_surface_material(0, material)
+	assert(Options.connect("shadow_setting_changed", self, "_on_shadow_setting_changed") == OK)
+	_on_shadow_setting_changed(Options.get("shadows"))
 
 
 func _on_RoomTracker_room_changed(area):
@@ -19,8 +21,6 @@ func _on_RoomTracker_room_changed(area):
 	var floor_book_parent = room.floor_books
 	if not floor_book_parent.is_a_parent_of(self):
 		self.call_deferred("_switch_parent", floor_book_parent)
-	assert(Options.connect("shadow_setting_changed", self, "_on_shadow_setting_changed") == OK)
-	_on_shadow_setting_changed(Options.get("shadows"))
 
 
 func _switch_parent(new_parent):
