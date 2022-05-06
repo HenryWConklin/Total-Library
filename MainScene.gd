c5487a62b739dee107d40442ac041c85ab3d3445
Henry Conklin <henrywconklin@gmail.com>
2022-05-05 21:18:31 -0700

    Fade to black transition to start game

--- MainScene.gd
+++ MainScene.gd
@@ -4,6 +4,11 @@ export(PackedScene) var main_scene
 
 
 func _on_MainMenu_start_game():
+	$AnimationPlayer.play("SceneTransition")
+
+
+# Called by animation player to transition between scenes
+func transition_scenes():
 	$MainMenu.queue_free()
 	$MainMenu.hide()
 	remove_child($MainMenu)
