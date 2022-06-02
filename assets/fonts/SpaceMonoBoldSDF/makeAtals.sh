c12c4511f64d9b376eccd5c5462ce181678b4257
Henry Conklin <henrywconklin@gmail.com>
2022-06-01 18:09:36 -0700

    Rename some files for windows compat

--- assets/fonts/SpaceMonoBoldSDF/makeAtals.sh
+++ assets/fonts/SpaceMonoBoldSDF/makeAtals.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Montage command from imagemagick
-montage {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}.png dot.png ",.png" "'.png" "?.png" "!.png" -geometry +2+2 -background none SpaceMonoBoldSDF.png
+montage {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}.png dot.png "comma.png" "apostrophe.png" "question.png" "bang.png" -geometry +2+2 -background none SpaceMonoBoldSDF.png
 
