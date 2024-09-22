14a789593039afdf0e16958b3e3ded47ace61b9f
Henry Conklin <henrywconklin@gmail.com>
2024-09-22 16:38:14 -0400

    Fix some lag spikes caused by shader compilation
    Mostly noticable on Firefox.
    
    Shader compilation seems to only happen on first render, so added
    some planes and dummy props behind the wall in the main menu with
    all the materials in the game. There's no visibility culling so
    as long as they're in the view frustum they'll get rendered and trigger
    the shader compilation right as the menu loads.

--- MainScene.gd
+++ MainScene.gd
@@ -1,6 +1,15 @@
 extends Node
 
 export(PackedScene) var main_scene
+export(NodePath) var held_book_preload_path: NodePath
+
+
+func _ready():
+	# Simulate picking up the book to force shader to compile in the menu instead of on first pickup.
+	# Should be the same as the shelf books but for some reason it's a different shader compile.
+	var book_text = BookRegistry.get_book_text(BookIndex.new())
+	var held_book_preload = get_node(held_book_preload_path)
+	held_book_preload.pull_from_shelf(book_text, Transform(), held_book_preload.global_transform)
 
 
 func _on_MainMenu_start_game():
